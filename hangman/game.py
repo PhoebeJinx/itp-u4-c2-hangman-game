fdd28c0f46bddd518b75a078abba5220cf6962e5
Devan Govender <devakumarangovender@gmail.com>
2017-08-08 19:00:18 +0000

    All tests passed

--- hangman/game.py
+++ hangman/game.py
@@ -65,19 +65,26 @@ def _uncover_word(answer_word, masked_word, character):
         return masked_word
 
 def guess_letter(game, letter):
-    guess = _uncover_word(game["answer_word"], game["masked_word"], letter)
-    
-    if guess == game["answer_word"]:
-        raise GameWonException 
-    
-    if guess == game["masked_word"]:
-        game["remaining_misses"] = game["remaining_misses"] - 1        
     
     if game["remaining_misses"] == 0:
-        raise GameLostException
+        raise GameFinishedException
+        
+    if game["answer_word"] == game["masked_word"]:
+        raise GameFinishedException 
     
+    guess = _uncover_word(game["answer_word"], game["masked_word"], letter)
+    
+    if guess == game["masked_word"]:
+        game["remaining_misses"] = game["remaining_misses"] - 1
+   
     game["masked_word"] = guess
-    game["previous_guesses"].append(letter.lower())
+    game["previous_guesses"].append(letter.lower()) 
+    
+    if guess == game["answer_word"]:
+        raise GameWonException
+     
+    if game["remaining_misses"] == 0:
+        raise GameLostException
     
     return game
     
